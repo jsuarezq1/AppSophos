7b063fd9b7412ebd3cc2fe1b6d03fe2d5234fdf4
Jorge <jsuarezq1@gmail.com>
2022-01-07 21:37:49 -0500

    commit con Mapa y zoom

--- app/src/main/java/com/sophos/myappr/MapsActivity.kt
+++ app/src/main/java/com/sophos/myappr/MapsActivity.kt
@@ -1,7 +1,16 @@
 package com.sophos.myappr
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.LocationRequest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.PackageManagerCompat.LOG_TAG
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.location.FusedLocationProviderClient
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -10,14 +19,19 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.sophos.myappr.databinding.ActivityMapsBinding
+import java.net.URI.create
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private val TAG2 = "MapsActivity"
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -38,11 +52,39 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * installed Google Play services and returned to the app.
      */
     override fun onMapReady(googleMap: GoogleMap) {
+
+        val viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        val TAG3 = "MapsActivity"
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val bogota = LatLng(4.679, -74.044)
-        mMap.addMarker(MarkerOptions().position(bogota).title("Marker in Bogota"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(bogota))
+        var latitud: Double
+        var longitud: Double
+        var nombre: String
+        var punto= LatLng(4.679, -74.044)
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(punto))
+        mMap.setMinZoomPreference(6.0f)
+
+        viewModel.getPosts()
+        viewModel.myResponseList.observe(this, Observer {
+            if (it != null) {
+                for (city in it) {
+
+                    for (i in 0..7) {
+                        Log.d(TAG2, city.Items[i].IdOficina.toString())
+                        Log.d(TAG2, city.Items[i].Ciudad.toString())
+                        latitud = city.Items[i].Latitud.toDouble()
+                        longitud = city.Items[i].Longitud.toDouble()
+                        nombre = city.Items[i].Nombre.toString()
+
+                        punto= LatLng(latitud, longitud)
+                        mMap.addMarker(MarkerOptions().position(punto).title(nombre))
+
+                    }
+                }
+
+            }
+        })
     }
 }
